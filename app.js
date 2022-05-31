147ab6df6927ff76ae49894451cc9b33c60c6594
Amit Mahato <amit643.mahato@gmail.com>
2022-05-31 17:26:23 +0545

    core: app.yaml to deploy to App Engine

--- app.js
+++ app.js
@@ -5,13 +5,20 @@ import { CORSMiddleware } from "./middleware/index.js";
 
 dotenv.config(); // load environment variables from .env file
 
+const WHITE_LISTED_URLS = [];
+if (process.env.NODE_ENV !== "local") {
+  WHITE_LISTED_URLS.push(process.env.UI_URL);
+} else {
+  WHITE_LISTED_URLS.push("http://localhost:3000");
+}
+
 const app = express();
 export default app;
 
 app.use(express.static("public"));
-app.use(CORSMiddleware); // Handle CORS
+app.use((req, res, next) => CORSMiddleware(req, res, next, WHITE_LISTED_URLS)); // Handle CORS
 app.use(routes);
 
-app.listen(process.env.SERVER_PORT, () =>
-  console.log(`Listening on port ${process.env.SERVER_PORT}...`)
+app.listen(process.env.PORT, () =>
+  console.log(`Listening on port ${process.env.PORT}...`)
 );
--- routes.js
+++ routes.js
@@ -3,5 +3,8 @@ import { getPing, postImage } from "./controller/index.js";
 
 export const routes = express.Router();
 
+routes.get("", (_, res) => {
+  res.status(200).send("Server is up and running ✅");
+});
 routes.get("/ping", getPing);
 routes.post("/upload", postImage);
